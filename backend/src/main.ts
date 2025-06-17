a287a10655343da74fe07b0d96fafbd82fcfa0c1
simarjot <simarjot0032@gmail.com>
2025-06-18 01:22:35 +0530

    updating the cros

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
+  app.enableCors({
+    origin: '*', // Allow all origins
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'], // Allow these HTTP methods
+    allowedHeaders: ['Content-Type', 'Authorization'], // Allow these headers
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
